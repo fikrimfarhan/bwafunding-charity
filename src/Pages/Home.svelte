fc99e0f9199c681025f2d2e238019b132fe8b45f
Fikri M Farhan <fikrimuhammadfarhan@gmail.com>
2021-01-07 19:03:57 +0700

    Convert HTML to svelte Components

---
+++ src/Pages/Home.svelte
@@ -0,0 +1,20 @@
+<script>
+	
+	import CharityList from "../Components/CharityList.svelte";
+	import Header from '../Components/Header.svelte';
+	import Welcome from '../Components/Welcome.svelte';
+	import Promo from '../Components/Promo.svelte';
+	import Footer from '../Components/Footer.svelte';
+
+	import	{ charities } from "../data/charities.js";
+	let title = "Charity";
+	
+
+	
+</script>
+
+<Header/>
+<Welcome/>
+<CharityList {charities}/>
+<Promo/>
+<Footer/>
---
+++ src/data/charities.js
@@ -0,0 +1,15 @@
+module.exports = {
+    charities: [{
+        id: 1,
+        title: 'First Charity Project',
+        category: 'Money',
+        thumbnail: 'https://live.staticflickr.com/4098/4933983235_c3a77737ac_b.jpg',
+        pledged: 90000,
+        target: 100000,
+        date_end: +new Date('10 August 2021'),
+        profile_photo: 'https://live.staticflickr.com/4027/4357722810_7136f4a9e9_s.jpg',
+        profile_name: 'Viroide Bueno',
+        no_pledges: 0
+
+    }]
+}
