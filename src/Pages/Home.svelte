0de64275c5a674494bad7daffede4ec0c26323c6
Fikri M Farhan <fikrimuhammadfarhan@gmail.com>
2021-01-07 19:49:01 +0700

    Convert HTML to svelte Components and Home

--- src/Pages/Home.svelte
+++ src/Pages/Home.svelte
@@ -1,10 +1,10 @@
 <script>
 
-	import CharityList from "../components/CharityList.svelte";
-	import Header from '../components/Header.svelte'
-	import Welcome from '../components/Welcome.svelte'
-	import Promo from '../components/Promo.svelte'
-	import Footer from '../components/Footer.svelte'
+	import CharityList from "../Components/CharityList.svelte";
+	import Header from '../Components/Header.svelte'
+	import Welcome from '../Components/Welcome.svelte'
+	import Promo from '../Components/Promo.svelte'
+	import Footer from '../Components/Footer.svelte'
 	import	{ charities } from "../data/charities.js";
 	let title = "Charity";
 	
